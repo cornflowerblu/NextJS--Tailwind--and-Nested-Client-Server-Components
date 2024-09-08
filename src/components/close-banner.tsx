a1ed6b51854f0e028f8ea61338c8e8a3eac875e3
Roger Urich <rogerdurich+git@gmail.com>
2024-09-08 03:14:44 -0700

    one last tiny refactor don't need a class on the banner button

--- src/components/close-banner.tsx
+++ src/components/close-banner.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import closeIcon from "@/images/close.svg";
-import { BannerProps } from "./banner";
+import { BannerProps } from "@/components/banner";
 
 type CloseBannerButtonProps = Pick<BannerProps, "onClose">;
 
 const CloseBannerButton: React.FC<CloseBannerButtonProps> = ({ onClose }) => {
   return (
-    <button className="close-banner" onClick={onClose}>
+    <button onClick={onClose}>
       <Image src={closeIcon} alt="Close banner" />
     </button>
   );
